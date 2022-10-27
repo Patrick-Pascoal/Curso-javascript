ed7c0523ec615f838f0627ce911f1e68eae3f095
Patrick Pascoal <patrick.7000@hotmail.com>
2022-10-26 23:15:27 -0300

    aula 201-1

---
+++ src/pages/201/201.tsx
@@ -0,0 +1,23 @@
+export function Index(treino: any) {
+  return (
+    <div>
+      <h1>ola dfasdf </h1>
+    </div>
+  );
+}
+
+export function Trening(teino1: any) {
+  var nome = "andre";
+  var possuiFaculdade = true;
+
+  console.log(nome, possuiFaculdade, nome);
+
+  var nome = "patrick",
+    idade = 18,
+    comidaFavorita = "pizza",
+    time = "vasco";
+
+  console.log(nome, idade, comidaFavorita, time);
+
+  return <div>comidaFavorita</div>;
+}

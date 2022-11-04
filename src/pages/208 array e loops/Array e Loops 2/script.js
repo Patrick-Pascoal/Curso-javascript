3e7a7de19c02746b92fb8a28d4e9cfe91d90b450
Patrick Pascoal <patrick.7000@hotmail.com>
2022-11-03 23:03:31 -0300

    aula 208-3

---
+++ src/pages/208 array e loops/Array e Loops 2/script.js
@@ -0,0 +1,14 @@
+var videoGame=["switch", "PS4","exbox","3ds"]
+
+for( var item=0 ; item<videoGame.length; item++) {
+    console.log(videoGame[item])
+if(videoGame[item]==="PS4"){break}
+}
+
+
+var frutas=["banana", "laranja","pera","uva"];
+
+frutas.forEach(function(fruta,index){
+frutas.pop(); // vai remover o ultimo da lista
+    
+    console.log(frutas)})

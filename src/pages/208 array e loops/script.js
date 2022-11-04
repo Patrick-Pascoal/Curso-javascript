9a699f54a34a09f18b1c5795b9ce06bb8a4dd851
Patrick Pascoal <patrick.7000@hotmail.com>
2022-11-03 23:02:43 -0300

    aula 208

---
+++ src/pages/208 array e loops/script.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+var videoGames=["switch", "Ps4" , "xbox"];
+videoGames.pop(); // comando que remove o ultimo item da array
+videoGames.push("3ds")  //nesse caso esta adicionando mais 1 item na array

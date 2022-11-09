f9818fd9fbb02e30bf6ec409c2f486b03680bc44
Patrick Pascoal <patrick.7000@hotmail.com>
2022-11-08 21:37:28 -0300

    Atribuição e ternario

---
+++ src/pages/atribuição e ternario/script.js
@@ -0,0 +1,21 @@
+var numero=10
+
+numero+=10    // esta semdo igual a numero=numero+10
+console.log(numero)
+
+
+
+var podeBeber;
+var idade=18;
+podeBeber=idade>=18 ? "pode": "não pode";  //  pode e como se fosse verdadeiro e não pode como se fosse falso 
+
+console.log(podeBeber)    // como teve uma condição acima o valor retornado vai ser o "pode"
+
+
+var notas;
+var media=12
+var baixa =baixa<media
+
+notas=media>=12 ? "passou": "não Passou";
+
+console.log(notas)

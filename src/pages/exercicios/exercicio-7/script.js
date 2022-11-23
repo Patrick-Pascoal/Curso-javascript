7f077819de0a5bcdf987ef83204fa708f5d0c4d6
Patrick Pascoal <patrick.7000@hotmail.com>
2022-11-22 23:11:41 -0300

    exercicio -7

--- src/pages/exercicios/exercicio-7/script.js
+++ src/pages/exercicios/exercicio-7/script.js
@@ -1,37 +1,18 @@
-//Escreva algoritimo que receba nome e sexo e no final infrome se e homem ou muler e no final mostre a quantidade de home e muler
+//Escreva algoritimo que receba nome e sexo e no final infrome se e homem ou mulher e no final mostre a quantidade de home e muler
 
-function sexoMF(cadastro) {
-    let masculino = []
-    let feminino = []
-    let i
-    // let feminino = []
-    cadastro.forEach((item, index) => {
-        // console.log(nome[1])
-        i = index % 2
-        if (i === 0) {
-            masculino[index] = item
-        } else {
-            feminino[index] = item
-        }
-        //console.log(masculino)
-        // console.log(index)
-    })
-    return masculino
+function nomeSexo(feminino, masculino) {
+    let mas = 0
+    let fem = 0
+
+    for (let i = 0; i <= masculino.length; i++) {
+        mas = i
+    }
+    feminino.forEach((item) => {
+        if (item != 0) { fem += 1 }
+
+    }); return "Feminino=" + fem + " " + "Masculino=" + mas
 }
 
-
-let nome = ["patrick", "masculino", "lorraine", "feminino", "kelvi", "masculino"]
-
-console.log(sexoMF(nome))
-/*
-
-let lista = ["lorraine", "feminino", "patrick", "Masculino"]
-
-console.log(lista.includes("feminino", 1))
-
-
-console.log(lista[0 + 2])*/
-
-
-let sma = 4 % 2
-console.log(sma)
+let sexoFeminino = ["ana", "ellen"]
+let sexoMasculino = ["luiz", "izaias", "Rafael"]
+console.log(nomeSexo(sexoFeminino, sexoMasculino))

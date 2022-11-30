708ee77d3ef7e39d285a2ddb6122701e158690f8
Patrick Pascoal <patrick.7000@hotmail.com>
2022-11-29 21:59:41 -0300

    exercicio 1

---
+++ src/pages/exercicios por/exercicio-1/script.js
@@ -0,0 +1,11 @@
+//1. Faça um algoritmo que receba dois números e exiba o resultado da sua soma.
+
+
+function sum(a, b) {
+    const result = a + b
+    return result
+}
+
+const result = sum(2, 2)
+
+console.log(result)

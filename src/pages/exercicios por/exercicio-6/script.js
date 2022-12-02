bd64c04de1ea68a1c6c168641a4e374c6bfc9936
Patrick Pascoal <patrick.7000@hotmail.com>
2022-12-02 20:55:28 -0300

    exercicio 6

---
+++ src/pages/exercicios por/exercicio-6/script.js
@@ -0,0 +1,11 @@
+//6. Ler dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a 
+//variável A passe a possuir o valor da variável B e a variável B passe a possuir o valor da
+// variável A. Apresentar os valores trocados.
+
+function exchangeValue(a, b) {
+    return b + " " + a
+}
+
+const result = exchangeValue(2, 3)
+
+console.log(result)

378a702bd8ad8c6540ed030697787b0984335942
Patrick Pascoal <patrick.7000@hotmail.com>
2022-12-07 23:32:12 -0300

    exercicio 25

---
+++ src/pages/exercicios por/exercicio-25/script.js
@@ -0,0 +1,12 @@
+//25. Faça um algoritmo que leia dois números e identifique se são iguais ou diferentes.
+//Caso eles sejam iguais imprima uma mensagem dizendo que eles são iguais. Caso sejam diferentes, 
+//informe qual número é o maior, e uma mensagem que são diferentes.
+
+
+function numberDifference(numberA, numberB) {
+    if (numberA > numberB) { return "valor A e maior" } else if
+        (numberB > numberA) { return "Valor B e maior" } else if (numberA === numberB) { return "São iguais" }
+
+
+}
+console.log(numberDifference(72, 72))
